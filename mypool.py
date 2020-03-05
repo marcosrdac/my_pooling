fc9c485b3493c51b00e1734139257a880ce2e8c2
Marcos Conceição <marcosrdac@gmail.com>
2020-03-05 17:10:29 -0300

    finalized.

--- mypool.py
+++ mypool.py
@@ -3,58 +3,20 @@ import numpy as np
 import netCDF4 as nc
 from PIL import Image
 import matplotlib.pyplot as plt
+from overlapping_pool import overlapping_pool
 
-
-def pool(img, whs=2, pool_func=np.std, pool_func_kwargs={}):
-    # allocating
-    ws = 2*whs
-    rows, cols = img.shape
-    assert not (ws>rows and ws>cols)
-    pool_rows = rows//ws + (rows-whs)//ws
-    pool_cols = cols//ws + (cols-whs)//ws
-    extra_col, extra_row = 0, 0
-    if rows%whs != 0 and (rows-whs)%whs != 0: extra_row=1
-    if cols%whs != 0 and (cols-whs)%whs != 0: extra_col=1
-    pooling_layer = np.empty((pool_rows+extra_row, pool_cols+extra_col))
-
-    for xpi in range(pool_cols):
-        xi = whs*xpi
-        xf = xi + ws
-        for ypi in range(pool_rows):
-            yi = whs*ypi
-            yf = yi + ws
-            subimg = img[yi:yf,xi:xf]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-        if extra_row != 0:
-            ypi += 1
-            subimg = img[-ws:,xi:xf]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-
-    if extra_col != 0:
-        xpi += 1
-        for ypi in range(pool_rows):
-            yi = whs*ypi
-            yf = yi + ws
-            subimg = img[yi:yf,-ws:]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-    return(pooling_layer)
-
-
-
+img = np.asarray(Image.open('img.jpg'))
 whs = 64
 pool_func = np.ptp
-img = np.asarray(Image.open('img.jpg'))
 
-
-img_pool0 = pool(img[:,:,0], whs, pool_func)
+img_pool0 = overlapping_pool(img[:,:,0], whs, pool_func)
 img_pool = np.empty((img_pool0.shape[0], img_pool0.shape[1], 3))
-img_pool[:,:,0] = img_pool0
-del img_pool0
-img_pool[:,:,1] = pool(img[:,:,1], whs, pool_func)
-img_pool[:,:,2] = pool(img[:,:,2], whs, pool_func)
+img_pool[:,:,0] = img_pool0; del(img_pool0)
+img_pool[:,:,1] = overlapping_pool(img[:,:,1], whs, pool_func)
+img_pool[:,:,2] = overlapping_pool(img[:,:,2], whs, pool_func)
+
+# rescaling needed for RGB plotting
 img_pool /= np.max(img_pool)
-#print(np.max(img_pool))
-#print(img_pool)
 
 fig, axes = plt.subplots(2,1)
 axes[0].imshow(img)
--- netcdf.py
+++ netcdf.py
@@ -3,44 +3,12 @@ import numpy as np
 import netCDF4 as nc
 from PIL import Image
 import matplotlib.pyplot as plt
+from overlapping_pool import overlapping_pool
 
 
-def pool(img, whs=2, pool_func=np.std, pool_func_kwargs={}):
-    # allocating
-    ws = 2*whs
-    rows, cols = img.shape
-    assert not (ws>rows and ws>cols)
-    pool_rows = rows//ws + (rows-whs)//ws
-    pool_cols = cols//ws + (cols-whs)//ws
-    extra_col, extra_row = 0, 0
-    if rows%whs != 0 and (rows-whs)%whs != 0: extra_row=1
-    if cols%whs != 0 and (cols-whs)%whs != 0: extra_col=1
-    pooling_layer = np.empty((pool_rows+extra_row, pool_cols+extra_col))
-
-    for xpi in range(pool_cols):
-        xi = whs*xpi
-        xf = xi + ws
-        for ypi in range(pool_rows):
-            yi = whs*ypi
-            yf = yi + ws
-            subimg = img[yi:yf,xi:xf]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-        if extra_row != 0:
-            ypi += 1
-            subimg = img[-ws:,xi:xf]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-
-    if extra_col != 0:
-        xpi += 1
-        for ypi in range(pool_rows):
-            yi = whs*ypi
-            yf = yi + ws
-            subimg = img[yi:yf,-ws:]
-            pooling_layer[ypi, xpi] = pool_func(subimg, **pool_func_kwargs)
-    return(pooling_layer)
-
-
-img_fp = '/home/marcosrdac/projects/oil_spill/netcdf/S1A_IW_SLC__1SDV_20181008T052755_20181008T052822_024040_02A081_3524_deb_Orb_ML_msk.nc'
+img_fp = '''\
+/home/marcosrdac/projects/oil_spill/netcdf/S1A_IW_SLC__1SDV_20181008T052755_20181008T052822_024040_02A081_3524_deb_Orb_ML_msk.nc\
+'''
 img = nc.Dataset(img_fp)['Intensity_VV_db']
 print(img.shape)
 
@@ -51,7 +19,7 @@ def pool_func(x):
     except:
         val = np.var(x)
     return(val)
-img_pool = pool(img, whs, pool_func)
+img_pool = overlapping_pool(img, whs, pool_func)
 
 fig, axes = plt.subplots(2,1)
 axes[0].imshow(img)
