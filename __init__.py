73df44d8ec028a9b26e9a16c93969621e9bd8c95
marcosrdac <marcosrdac@gmail.com>
2020-04-07 18:43:12 -0300

    making it a propper submodule

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def overlapping_pool(img, whs=2, pool_func=np.std, give_window=False, pool_func_kw={}):
+def overlapping_pool(img, whs=2, pool_func=np.std,
+                     give_window=False, pool_func_kw={}):
     '''
     Function made to create pooling layers with any pooling function, which is
     run at windows with side `ws` and half-side `whs`. The windows overlap at
@@ -16,13 +17,15 @@ def overlapping_pool(img, whs=2, pool_func=np.std, give_window=False, pool_func_
     '''
     ws = 2*whs
     rows, cols = img.shape
-    assert not (ws>rows and ws>cols)
+    assert not (ws > rows and ws > cols)
     # pooling layer allocation
     pool_rows = rows//ws + (rows-whs)//ws
     pool_cols = cols//ws + (cols-whs)//ws
     extra_col, extra_row = 0, 0
-    if rows%whs != 0: extra_row=1
-    if cols%whs != 0: extra_col=1
+    if rows % whs != 0:
+        extra_row = 1
+    if cols % whs != 0:
+        extra_col = 1
     pooling_layer = np.empty((pool_rows+extra_row, pool_cols+extra_col))
     # pooling function calcylated at every window
     for xpi in range(pool_cols):
@@ -31,22 +34,25 @@ def overlapping_pool(img, whs=2, pool_func=np.std, give_window=False, pool_func_
         for ypi in range(pool_rows):
             yi = whs*ypi
             yf = yi + ws
-            subimg = img[yi:yf,xi:xf]
-            window_kw = {'window':((yi,yf),(xi,xf))} if give_window else {}
-            pooling_layer[ypi, xpi] = pool_func(subimg, **window_kw, **pool_func_kw)
+            subimg = img[yi:yf, xi:xf]
+            window_kw = {'window': ((yi, yf), (xi, xf))} if give_window else {}
+            pooling_layer[ypi, xpi] = pool_func(
+                subimg, **window_kw, **pool_func_kw)
         # if extra row, force the calculation on its cells
         if extra_row != 0:
             ypi += 1
-            subimg = img[-ws:,xi:xf]
-            window_kw = {'window':((yi,yf),(xi,xf))} if give_window else {}
-            pooling_layer[ypi, xpi] = pool_func(subimg, **window_kw, **pool_func_kw)
+            subimg = img[-ws:, xi:xf]
+            window_kw = {'window': ((yi, yf), (xi, xf))} if give_window else {}
+            pooling_layer[ypi, xpi] = pool_func(
+                subimg, **window_kw, **pool_func_kw)
     # if extra col, force the calculation on its cells
     if extra_col != 0:
         xpi += 1
-        for ypi in range(pool_rows):
+        for ypi in range(pool_rows+extra_row):
             yi = whs*ypi
             yf = yi + ws
-            subimg = img[yi:yf,-ws:]
-            window_kw = {'window':((yi,yf),(xi,xf))} if give_window else {}
-            pooling_layer[ypi, xpi] = pool_func(subimg, **window_kw, **pool_func_kw)
+            subimg = img[yi:yf, -ws:]
+            window_kw = {'window': ((yi, yf), (xi, xf))} if give_window else {}
+            pooling_layer[ypi, xpi] = pool_func(
+                subimg, **window_kw, **pool_func_kw)
     return(pooling_layer)
